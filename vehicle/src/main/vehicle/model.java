ea785cd4119305b51f45e503646d10f4f59f44c6
Abdalla Roshdy <119878280+Abdalla172@users.noreply.github.com>
2022-12-16 15:55:23 +0200

    Add files via upload

---
+++ vehicle/src/main/vehicle/model.java
@@ -0,0 +1,9 @@
+package com.mycompany.vehicle;
+
+public class model {
+
+    public static void main(String[] args) {
+       Bus Ford=new Bus("Ford","Black",2004," E450 ","Hybrid");
+       Ford.busDetails();
+    }
+}
