479ee124935db2d1ad135a572fe2e9842dbce3ad
Abdalla Roshdy <119878280+Abdalla172@users.noreply.github.com>
2022-12-16 15:45:36 +0200

    Delete vehicle directory

--- vehicle/src/main/vehicle/model.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.vehicle;
-
-public class model {
-
-    public static void main(String[] args) {
-       Bus mercedes=new Bus("Mercedes","gray",2020,"Citaro","Hybrid");
-       mercedes.busDetails();
-    }
-}
